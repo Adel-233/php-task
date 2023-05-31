bb5eb6992848379662311b2b4df4930623a27059
Adel-233 <128091357+Adel-233@users.noreply.github.com>
2023-05-31 20:48:48 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="log w-100 vh-100 d-flex flex-column justify-content-center align-items-center">
+            <h1 class="text-capitalize">log in</h1>  
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+            </div>
+                <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+            </div>
+        <button type="button" class="btn btn-primary btn-lg mt-3 text-capitalize">log in</button>
+        <h4 class="mt-3 text-uppercase">if don't have any account, please sing in</h4>
+    </div>
+
+    
+
+
+    <script src="js/bootstrap.js"></script>
+    <script src="js/index.js"></script>
+</body>
+</html>
